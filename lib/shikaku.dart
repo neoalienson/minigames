6cce549e86f09c5e5ca787be38e06b91cf4f9712
neo.alienson <neo@01man.com>
2023-03-28 23:44:05 +0800

    Add the base color back

--- lib/shikaku.dart
+++ lib/shikaku.dart
@@ -19,6 +19,7 @@ class _ShikakuGameState extends State<ShikakuGame> {
   final _logger = Logger('ShikakuGame');
   int colorIndex = 0;
   final List<Color> colors = [
+    Colors.white70,
     Colors.red,
     Colors.green,
     Colors.blue,
