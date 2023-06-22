f7d0395dd96ac78dc68cd63df7cf4e1a4cdcfaa5
Neo Alienson <neo@01man.com>
2023-06-22 18:59:41 +0800

    refactor

--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -13,13 +13,22 @@ class MainMenuPage extends StatefulWidget {
   _MainMenuPageState createState() => _MainMenuPageState();
 }
 
+class MenuItem {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final Widget Function(BuildContext) function;
+
+  MenuItem(this.icon, this.title, this.subtitle, this.function);  
+}
+
 class _MainMenuPageState extends State<MainMenuPage> {
   Widget signinButton(BuildContext context) {
     return FutureBuilder(
       future: Authentication.initializeFirebase(context: context),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
-          return Text('Error initializing Firebase');
+          return const Text('Error initializing Firebase');
         } else if (snapshot.connectionState == ConnectionState.done) {
           return GoogleSignInButton();
         }
@@ -34,79 +43,44 @@ class _MainMenuPageState extends State<MainMenuPage> {
 
   @override
   Widget build(BuildContext context) {
+    var widgets = <Widget>[signinButton(context)];
+    final menu = <MenuItem>[
+        MenuItem(Icons.gamepad, 'Tic Tac Toe', 'A classic game of Xs and Os', 
+          (context) => const TicTacToePage()),
+        MenuItem(Icons.gamepad, 'Tic Tac Toe 3 players in 4 x 4', 'A modified game in 4 x 4 Grid for 3 players', 
+          (context) => TicTacToe3x4Page()),
+        MenuItem(Icons.gamepad, 'Shikaku', 'Shikaku',
+          (context) => ShikakuGameMenu()),
+        MenuItem(Icons.gamepad, 'Hamster Race', '',
+          (context) => HamsterRaceScreen()),
+        MenuItem(Icons.gamepad, 'Whack a Hamster', '',
+          (context) => WhackAMoleGame()),
+      ];
+
+    for (var i in menu) {
+      widgets.add(
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: i.function),
+              );
+            },
+            child: ListTile(
+              leading: Icon(i.icon),
+              title: Text(i.title),
+              subtitle: Text(i.subtitle),
+            ),
+          ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Game Menu'),
+        title: const Text('Game Menu'),
       ),
       body: ListView(
-        children: [
-          signinButton(context),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => TicTacToePage()),
-              );
-            },
-            child: ListTile(
-              leading: Icon(Icons.gamepad),
-              title: Text('Tic Tac Toe'),
-              subtitle: Text('A classic game of Xs and Os'),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => TicTacToe3x4Page()),
-              );
-            },
-            child: ListTile(
-              leading: Icon(Icons.gamepad),
-              title: Text('Tic Tac Toe 3 players in 4 x 4'),
-              subtitle: Text('A modified game in 4 x 4 Grid for 3 players'),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ShikakuGameMenu()),
-              );
-            },
-            child: ListTile(
-              leading: Icon(Icons.gamepad),
-              title: Text('Shikaku'),
-              subtitle: Text('Shikaku'),
-            ),
-          ),                  
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HamsterRaceScreen()),
-              );
-            },
-            child: ListTile(
-              leading: Icon(Icons.gamepad),
-              title: Text('Hamster Race'),
-              subtitle: Text(''),
-            ),
-          ),  
-           GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => WhackAMoleGame()),
-              );
-            },
-            child: ListTile(
-              leading: Icon(Icons.gamepad),
-              title: Text('whack a hamster'),
-              subtitle: Text(''),
-            ),
-          ),  
-        ],
+        children: widgets,
       ),
     );
   }
